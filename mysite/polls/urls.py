6022ecd8f45a762b46a864b8d715d714b2aaee43
vikram <vikram.aroskar@gmail.com>
2020-10-22 14:18:35 +0530

    add bootstrap 4 and dtl

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('page1/', views.page1, name='page1'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def page1(request):
+    return render(request, 'page1.html')
